63197f364b4a0eae4f769ac43230f356d3177a95
amir sadough <85782806+amirsadough94@users.noreply.github.com>
2024-03-19 11:53:27 +0330

    Update compile_time_map.cc

--- compile_time_map/compile_time_map.cc
+++ compile_time_map/compile_time_map.cc
@@ -3,6 +3,11 @@
 #include <cstddef>
 #include <iostream>
 #include <string>
+#include <optional>
+
+struct NoneValue {
+    constexpr NoneValue(){}
+};
 
 template<std::size_t Size>
 struct LiteralString
@@ -15,54 +20,69 @@ struct LiteralString
 	char value[Size];
 };
 
-template<LiteralString Key, auto Value>
+template<LiteralString Key, auto Value = NoneValue{} >
 struct LiteralPair
 {
 	static constexpr auto key = Key;
-	static constexpr auto value = Value;
+	static constexpr decltype(Value) value = Value;
 };
-
-template<LiteralString Key,typename Value>
-struct LiteralKeyValue
-{
-	LiteralKeyValue(Value&& i_value):value(i_value){}
-	
-	static constexpr auto key = Key;
-	Value value;
-};
-
 
 template<typename KeyType, typename... Pairs>
 struct LiteralMap
 {
+private:
+    template<LiteralString Key>
+    struct LiteralKeyValue
+    {
+        LiteralKeyValue(KeyType&& i_value):value(i_value){}
+        LiteralKeyValue(NoneValue i_value){}
+
+        static constexpr auto key = Key;
+        std::optional<KeyType> value;
+    };
+public:
 
 	 LiteralMap():map(Pairs::value...){}
 
-	 std::tuple<LiteralKeyValue<Pairs::key,KeyType>...> map;
+	 std::tuple<LiteralKeyValue<Pairs::key>...> map;
 
 	 template<LiteralString Key>
-	 KeyType get(){
-	 	return std::get<LiteralKeyValue<Key,KeyType>>(map).value;
+	 KeyType get(KeyType&& default_value = KeyType()){
+	 	return std::get<LiteralKeyValue<Key>>(map).value.value_or(default_value);
 	 }
-
-
-	
+    
+    template<LiteralString Key>
+    void set(KeyType&& value ){
+        return std::get<LiteralKeyValue<Key>>(map).value = value;
+    }
+    
+    template<LiteralString Key>
+    bool contains(){
+        return std::get<LiteralKeyValue<Key>>(map).value.has_value();
+    }
+    
+    template<LiteralString Key>
+    void erase(){
+        return std::get<LiteralKeyValue<Key>>(map).value.reset();
+    }
 };
-
-
 
 int main()
 {
-	LiteralMap<std::size_t,LiteralPair<"a",1>,LiteralPair<"b",2>,LiteralPair<"c",3>> map;
+	LiteralMap<std::size_t,LiteralPair<"a",1>,LiteralPair<"b",2>,LiteralPair<"c",3>,LiteralPair<"d"> > map;
 
 	std::cout << "size of items stored is " <<  std::tuple_size<decltype(map.map)>::value << std::endl;
 
 	std::cout << "the value of a is " << map.get<"a">() << std::endl;
 	std::cout << "the value of b is " << map.get<"b">() << std::endl;
 	std::cout << "the value of c is " << map.get<"c">() << std::endl;
-
+    	std::cout << "the value of d is " << map.get<"d">(100) << std::endl;
+    	map.erase<"a">();
+    	std::cout << "has value of a after erasing " << map.contains<"a">() << std::endl;
+    
+    	std::cout << "itterating over all compiled keys... \n";
 	// you can also make more beautiful ;)
-	std::apply([](auto&&... args) {((std::cout << args.key.value << " => " << args.value << '\n'), ...);}, map.map);
+	std::apply([](auto&&... args) {((std::cout << args.key.value << " => " << args.value.value_or(100) << '\n'), ...);}, map.map);
 
 	return 0;
 };
